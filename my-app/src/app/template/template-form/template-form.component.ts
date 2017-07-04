364586893e51d83145bdc47ef1b0210af9985542
jonathannicolas <hello@gmail.com>
2017-07-04 11:40:05 +0200

    Add two way data-binding and diagnostic

--- my-app/src/app/template/template-form/template-form.component.ts
+++ my-app/src/app/template/template-form/template-form.component.ts
@@ -1,25 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+export class User {
+  name: string;
+  username: string;
+}
+
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
   styleUrls: ['./template-form.component.css']
 })
 export class TemplateFormComponent implements OnInit {
-  userName: string;
-  userUserame: string;
+  user: User;
 
 
   constructor() { }
 
   ngOnInit() {
+    this.user = {
+      name:'',
+      username:''
+
+    }
   }
 
   get diagnostic() {
-    return JSON.stringify({
-      name: this.userName,
-      username: this.userUserame
-    })
+    return JSON.stringify(this.user)
   }
 
 
